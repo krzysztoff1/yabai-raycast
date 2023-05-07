da0c151d7a8678d66010d301564afbfe447a49e1
Krzysztof <halidu13@gmail.com>
2023-05-07 15:43:00 +0200

    feat: start/stop yabai with homebrew

---
+++ src/start.tsx
@@ -0,0 +1,65 @@
+import { Toast, showHUD, showToast } from "@raycast/api";
+import { BREW_PATH, runShellScript } from ".";
+
+const start = async () => {
+  const cmd = `${BREW_PATH} services start yabai`;
+  return await runShellScript(cmd);
+};
+
+const restart = async () => {
+  try {
+    const cmd = `${BREW_PATH} services restart yabai`;
+    await runShellScript(cmd);
+  } catch (error) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: "Error",
+      message: "An error occurred while restarting Yabai.",
+    });
+  }
+};
+
+export default async () => {
+  try {
+    const { stdout, stderr } = await start();
+
+    if (stdout.includes("already started")) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Yabai",
+        message: "Yabai is already running. Restart?",
+        primaryAction: {
+          title: "Restart Yabai",
+          onAction: async () => {
+            await restart();
+            showToast({
+              style: Toast.Style.Success,
+              title: "Yabai",
+              message: "Yabai has been restarted.",
+            });
+          },
+        },
+      });
+
+      return;
+    }
+
+    if (stderr) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Error",
+        message: stderr,
+      });
+
+      return;
+    }
+
+    showHUD("Yabai has been started.");
+  } catch (error) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: "Error",
+      message: "An error occurred while starting Yabai.",
+    });
+  }
+};
---
+++ src/stop.tsx
@@ -0,0 +1,41 @@
+import { Toast, showHUD, showToast } from "@raycast/api";
+import { BREW_PATH, runShellScript } from ".";
+
+const start = async () => {
+  const cmd = `${BREW_PATH} services stop yabai`;
+  return await runShellScript(cmd);
+};
+
+export default async () => {
+  try {
+    const { stdout, stderr } = await start();
+
+    if (stdout.includes("not running")) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Yabai",
+        message: "Yabai is already stopped.",
+      });
+
+      return;
+    }
+
+    if (stderr) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Error",
+        message: stderr,
+      });
+
+      return;
+    }
+
+    showHUD("Yabai has been stopped.");
+  } catch (error) {
+    showToast({
+      style: Toast.Style.Failure,
+      title: "Error",
+      message: "An error occurred while stopping Yabai.",
+    });
+  }
+};
